7ed24a92a73d9f301b5eefe38e974c6f3877cadc
Alex G <gamezalexandra@live.com>
2020-07-12 13:09:34 -0600

    complex javascipt in jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
+const array = [1,4,9,16];
+
+class App extends Component {
+  render() {
+    const helloWorld = 'Welcome to the Road to learn React';
+    const element = <h1>Hello, world!</h1>
+    const user = {
+      firstName: 'Harper',
+      lastname: 'Perez'
+    };
+    return (
+      <div className="App">
+        {list.map(function(item) {
+          return <div>{item.title}</div>;
+        })}
+      </div>
+    );
+  }
+  
 }
 
 export default App;

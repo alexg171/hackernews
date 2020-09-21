f23439d891fb8fd386c44d59db984ff38f779e21
Alex Gamez <gamezalexandra@live.com>
2020-09-21 14:42:29 -0600

    amend prev

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ class App extends Component {
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedHits = this.state.list.filter(isNotId);
+    const updatedHits = this.state.result.hits.filter(isNotId);
     this.setState({ 
       result:  { ...this.state.result, hits: updatedHits }
     });
